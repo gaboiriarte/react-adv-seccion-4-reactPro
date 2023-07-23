cff88d3d11838b6e9dcc17a22f77ba234d497b94
Gabriel Iriarte <gabriel.iriarte@icloud.com>
2023-07-23 14:26:08 -0400

    Resolucion de tarea de rutas estaticas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -15,39 +15,25 @@ export const Navigation = () => {
         <nav>
           <img src={logo} alt="React Logo" />
           <ul>
-            <li>
-              <NavLink
-                to="/lazy1"
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Lazy 1
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/lazy2"
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Lazy 2
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/lazy3"
-                className={({ isActive }) => (isActive ? 'nav-active' : '')}
-              >
-                Lazy 3
-              </NavLink>
-            </li>
+            {routes.map(({ to, name }) => (
+              <li key={to}>
+                <NavLink
+                  to={to}
+                  className={({ isActive }) => (isActive ? 'nav-active' : '')}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
 
         <Routes>
-          <Route path="lazy1" element={<LazyPage1 />} />
-          <Route path="lazy2" element={<LazyPage2 />} />
-          <Route path="lazy3" element={<LazyPage3 />} />
+          {routes.map(({ Component, path }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
 
-          <Route path="/*" element={<Navigate to="/lazy1" replace />} />
+          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
         </Routes>
       </div>
     </BrowserRouter>
